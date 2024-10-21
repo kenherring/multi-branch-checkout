67bce6509397d00106c926b377ed300d6e588718
kenherring <kennethherring@gmail.com>
2024-10-21 03:55:10 -0400

    readme badges

--- src/commands.ts
+++ src/commands.ts
@@ -1,16 +1,15 @@
 import * as vscode from 'vscode'
-import { workerData } from "worker_threads"
 import { WorktreeFile, WorktreeFileGroup, WorktreeNode, WorktreeRoot } from "./worktreeView"
 
 
 export function commands_discardChanges(node: WorktreeNode) {
 	if (node instanceof WorktreeFile) {
-		vscode.commands.executeCommand('git.clean', { uri: node.uri })
+		return vscode.commands.executeCommand('git.clean', { uri: node.uri })
 	} else if (node instanceof WorktreeFileGroup) {
-		vscode.window.showWarningMessage('Not yet implemented')
+		return vscode.window.showWarningMessage('Not yet implemented')
 	} else if (node instanceof WorktreeRoot) {
-		vscode.window.showWarningMessage('Not yet implemented')
+		return vscode.window.showWarningMessage('Not yet implemented')
 	} else {
-		throw new Error('Unknown node type: ' + node)
+		throw new Error('Discard changes not supported for node type (node.id=' + node.id + ')')
 	}
 }
